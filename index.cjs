aae58faaa66698cf7eefd9d2054ed4e864bb7f11
Paulo Pimentel <fudevil123@gmail.com>
2024-09-24 22:11:49 -0300

    new

--- index.cjs
+++ index.cjs
@@ -3,26 +3,28 @@ const Stripe = require('stripe');
 const cors = require('cors');
 
 const app = express();
-const stripe = Stripe("sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg"); // Use a variável de ambiente para a chave secreta
+const stripe = Stripe("sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg");
 
 // Middleware CORS
 app.use(cors());
 
-// Middleware para capturar o corpo bruto da requisição apenas para o webhook
-app.use('/api/webhook', express.raw({ type: 'application/json' }));
-
-// Middleware para outros tipos de requisições
-app.use(express.json()); // Isso será usado para outras rotas
+// Middleware para capturar o corpo bruto da requisição
+app.use(express.json({
+  verify: (req, res, buf) => {
+    if (req.originalUrl.startsWith('/api/webhook')) {
+      req.rawBody = buf.toString(); // Armazena o corpo bruto para verificação
+    }
+  },
+}));
 
 // Endpoint do Webhook
 app.post('/api/webhook', async (req, res) => {
   const signature = req.headers['stripe-signature'];
-  const rawBody = req.body; // O corpo bruto é agora capturado pelo middleware express.raw()
+  const rawBody = req.rawBody; // Usa o corpo bruto armazenado
 
   let event;
   try {
-    // Aqui, estamos usando o corpo bruto
-    event = stripe.webhooks.constructEvent(rawBody, signature, "whsec_fflHYnGsltO55GQTlPT9HWOssiVKehQy"); // Use a variável de ambiente para a chave do webhook
+    event = stripe.webhooks.constructEvent(rawBody, signature, "whsec_fflHYnGsltO55GQTlPT9HWOssiVKehQy");
   } catch (err) {
     console.error('Webhook signature verification failed:', err);
     return res.status(400).send(`Webhook Error: ${err.message}`);
@@ -37,12 +39,6 @@ app.post('/api/webhook', async (req, res) => {
 
   // Retornar uma resposta ao Stripe
   res.json({ received: true });
-});
-
-// Outras rotas ou endpoints da API podem ser definidos aqui
-app.post('/api/some-other-endpoint', (req, res) => {
-  // Lógica para outro endpoint
-  res.json({ message: 'This is another endpoint' });
 });
 
 // Iniciar o servidor
