4a051d6a5ea6803a1c23c190039adf67887707ee
Paulo Pimentel <fudevil123@gmail.com>
2024-09-24 22:25:20 -0300

    new

--- index.cjs
+++ index.cjs
@@ -2,17 +2,19 @@ const express = require('express');
 const Stripe = require('stripe');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const webhookRoutes = require('./routes/webhook.cjs');
+const webhookRoutes = require('./routes/webhook');
 
 const app = express();
 const stripe = Stripe('sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg');
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Middleware para processar JSON
 
-// Define o middleware para o endpoint do webhook (deve ser antes da rota do webhook)
+// Define o middleware para o endpoint do webhook
 app.use('/webhook', express.raw({ type: 'application/json' }));
+
+// Middleware para processar JSON (antes do webhook)
+app.use(express.json());
 
 app.use('/api', webhookRoutes);
 
@@ -25,8 +27,10 @@ app.post('/create-checkout-session', async (req, res) => {
             line_items: [
                 {
                     price_data: {
-                        currency: 'usd',
-                        product_data: {
+                        currency:
+                            'usd',
+                        product_data:
+                        {
                             name: 'Nome do Produto',
                         },
                         unit_amount: 2000,
@@ -44,7 +48,8 @@ app.post('/create-checkout-session', async (req, res) => {
     } catch (error) {
         console.error('Error creating checkout session:', error.message);
         res.status(500).json({
-            error: 'Failed to create checkout session',
+            error:
+                'Failed to create checkout session'
         });
     }
 });
