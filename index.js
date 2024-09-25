0aea92c1441c488e041a32600fbe84084d38dca5
Paulo Pimentel <fudevil123@gmail.com>
2024-09-25 04:30:11 -0300

    new

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const stripe = require('stripe')('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');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Usando a chave secreta
 
-const endpointSecret = 'whsec_fflHYnGsltO55GQTlPT9HWOssiVKehQy';
+const endpointSecret = process.env.STRIPE_ENDPOINT_KEY; // Usando a chave do endpoint
 
 const port = 3000;
 
@@ -31,7 +31,7 @@ app.post('/checkout', async (req, res) => {
                         product_data: {
                             name: 'Preço de Quimplo - Template Pass',
                         },
-                        unit_amount: 50,
+                        unit_amount: 50, // 50 centavos
                     },
                     quantity: 1,
                 },
@@ -41,7 +41,6 @@ app.post('/checkout', async (req, res) => {
             cancel_url: 'http://localhost:5173/cancel',
         });
 
-        // Armazene o session_id no sessionStorage
         res.json({ id: session.id });
         console.log("Sessão de checkout criada com sucesso");
     } catch (error) {
@@ -55,7 +54,7 @@ app.post('/check-payment-status', async (req, res) => {
 
     try {
         const session = await stripe.checkout.sessions.retrieve(session_id);
-        const isPaid = session.payment_status === 'paid'; // ou 'complete', dependendo do seu fluxo
+        const isPaid = session.payment_status === 'paid';
 
         res.json({ isPaid });
     } catch (error) {
@@ -63,7 +62,6 @@ app.post('/check-payment-status', async (req, res) => {
         res.status(500).json({ error: 'Erro ao verificar o status do pagamento' });
     }
 });
-
 
 app.post('/webhooks', (req, res) => {
     const sig = req.headers['stripe-signature'];
@@ -75,7 +73,6 @@ app.post('/webhooks', (req, res) => {
         return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
-    // Handle the event
     if (event.type === 'checkout.session.completed') {
         const session = event.data.object;
         console.log('Pagamento confirmado:', session);
