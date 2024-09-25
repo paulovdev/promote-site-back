73f02c56aa58dae32f51a74d4389950c8725d6e9
Paulo Pimentel <fudevil123@gmail.com>
2024-09-25 03:48:37 -0300

    new

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const Stripe = require('stripe');
-const cors = require('cors');
-const webhookRoutes = require('./routes/webhook');
-const checkPaymentStatusRoutes = require('./routes/checkPaymentStatus');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const stripe = require('stripe')('sk_test_J...');
 
-const app = express();
-const stripe = Stripe('sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg');
+const endpointSecret = 'whsec_h...';
 
-app.use(cors());
+const port = 3000
 
-// Middleware para o webhook - deve estar antes do express.json()
-app.use('/api', express.raw({ type: 'application/json' }), webhookRoutes);
-app.use(express.json());
-app.use('/api', checkPaymentStatusRoutes);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/create-checkout-session', async (req, res) => {
+// parse application/json
+app.use(bodyParser.json({
+    verify: (req, res, buf) => {
+        req.rawBody = buf
+    }
+}))
+
+app.use(cors())
+
+app.post('/checkout', async (req, res) => {
     try {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
@@ -42,6 +48,51 @@ app.post('/create-checkout-session', async (req, res) => {
         res.status(500).json({ error: 'Failed to create checkout session' });
     }
 });
+app.post('/sub', async (req, res) => {
+    const { email, payment_method } = req.body;
 
-const PORT = process.env.PORT || 4242;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const customer = await stripe.customers.create({
+        payment_method: payment_method,
+        email: email,
+        invoice_settings: {
+            default_payment_method: payment_method,
+        },
+    });
+
+    const subscription = await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ plan: 'plan_G........' }],
+        expand: ['latest_invoice.payment_intent']
+    });
+
+    const status = subscription['latest_invoice']['payment_intent']['status']
+    const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+
+    res.json({ 'client_secret': client_secret, 'status': status });
+})
+
+app.post('/webhooks', (req, res) => {
+    const sig = req.headers['stripe-signature'];
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
+    }
+    catch (err) {
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    // Handle the event
+    if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+
+        console.log('Pagamento confirmado:', session);
+    }
+
+    res.json({ received: true });
+});
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
