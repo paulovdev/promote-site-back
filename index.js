4ed8bb7d5ba38c6bf4a300b0a5288c325781a345
Paulo Pimentel <fudevil123@gmail.com>
2024-09-24 23:10:13 -0300

    new

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const Stripe = require('stripe');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const webhookRoutes = require('./routes/webhook');
 
 const app = express();
-const stripe = Stripe('sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg'); // Sua chave secreta do Stripe
+const stripe = Stripe('sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg');
 
 // Middleware
 app.use(cors());
-app.use('/api/webhook', express.raw({ type: 'application/json' })); // Middleware para o webhook
-app.use('/api', webhookRoutes); // Adiciona as rotas do webhook sob /api
+app.use(express.json()); // Middleware para processar JSON
+
+// Middleware específico para o webhook
+app.use('/api/webhook', express.raw({ type: 'application/json' })); // Certifique-se de que o caminho está correto
+app.use('/api', webhookRoutes); // Adiciona as rotas do webhook
 
 // Rota para criar uma sessão de checkout
 app.post('/create-checkout-session', async (req, res) => {
