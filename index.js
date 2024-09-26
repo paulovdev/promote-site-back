0ba830445614f4631e039d6fa9a778a4eab0c5f1
Paulo Pimentel <fudevil123@gmail.com>
2024-09-26 20:43:34 -0300

    new

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-// server.js
+const stripe = require('stripe')('sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg');
 const express = require('express');
-const { json } = require('body-parser');
-const stripe = require('stripe')("sk_test_51Q1x2cRraDIE2N6qLbzeQgMBnW5xSG7gCB6W3tMxCfEWUz8p7vhjnjCAPXHkT2Kr50i6rgAC646BmqglaGWp5dhd00SZi9vWQg");
-const cors = require('cors');
-
 const app = express();
-const PORT = process.env.PORT || 4242;
+app.use(express.static('public'));
 
-app.use(cors());
-app.use(json());
+const YOUR_DOMAIN = 'http://localhost:5173';
 
-app.post('/create-payment-intent', async (req, res) => {
-  const { amount } = req.body; // Certifique-se de que o valor está em centavos
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: 'usd', // ou a moeda que você estiver usando
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: 'price_1Q1ylSRraDIE2N6q1CPEIbBT',
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}?success=true`,
+        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
-    res.send({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
+
+    res.redirect(303, session.url);
 });
 
-app.post('/check-payment-status', async (req, res) => {
-  const { session_id } = req.body;
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.retrieve(session_id);
-    res.send({ isPaid: paymentIntent.status === 'succeeded' });
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(4242, () => console.log('Running on port 4242'));
