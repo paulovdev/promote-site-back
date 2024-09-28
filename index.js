2c16668dd654c07b097679190209ca256c1451f1
Paulo Pimentel <fudevil123@gmail.com>
2024-09-28 08:06:35 -0300

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const stripe = require('stripe')('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');
+const stripe = require('stripe')('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');
 
 const app = express();
 const endpointSecret = 'whsec_jqLnO8OzKJYeYaHrh1W7O2GDvMUNbPyf';
@@ -19,7 +19,7 @@ app.post('/create-checkout-session', async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: [
                 {
-                    price: 'price_1Q3riRRraDIE2N6qjxIVJMcC',
+                    price: 'price_1Q3yQTFQSsXyaRCdJZjMxU9Y',
                     quantity: 1,
                 },
             ],
